f79baddbe481347d4d6bb19fbec9387770a65f86
sugonhwang <sugonhwang1@gmail.com>
2025-09-26 21:13:12 +0900

    move: 폴더 경로 수정

--- todolist/TodoList.js
+++ todolist/TodoList.js
@@ -1,21 +1,51 @@
 let currentDate = new Date();
 
 let dateEl = document.getElementById("current-date");
-const prevButton = document.getElementById("prev-button");
-const nextButton = document.getElementById("next-button");
+let formEl = document.getElementById("form-current-date");
+const addBtn = document.getElementById("add-btn");
 
-function updateDate() {
-  const options = { weekday: "long", day: "numeric", month: "short" };
-  dateEl.textContent = currentDate.toLocaleDateString("en-GB", options);
-}
-prevButton.addEventListener("click", () => {
+// 이전 날짜로 이동
+document.getElementById("prev-btn").addEventListener("click", () => {
   currentDate.setDate(currentDate.getDate() - 1);
-  updateDate();
+  updateDateTitle();
 });
 
-nextButton.addEventListener("click", () => {
+// 다음 날짜로 이동
+document.getElementById("next-btn").addEventListener("click", () => {
   currentDate.setDate(currentDate.getDate() + 1);
-  updateDate();
+  updateDateTitle();
 });
 
-updateDate();
+// 테스크 폼: 이전 날짜로 이동
+document.getElementById("form-prev-btn").addEventListener("click", () => {
+  currentDate.setDate(currentDate.getDate() - 1);
+  formUpdateDateTitle();
+});
+
+// 테스크 폼: 다음 날짜로 이동
+document.getElementById("form-next-btn").addEventListener("click", () => {
+  currentDate.setDate(currentDate.getDate() + 1);
+  formUpdateDateTitle();
+});
+
+function updateDateTitle() {
+  const options = {
+    weekday: "short",
+    day: "numeric",
+    month: "short",
+  };
+  dateEl.textContent = currentDate.toLocaleDateString("en-US", options);
+}
+
+function formUpdateDateTitle() {
+  const options = {
+    weekday: "short",
+    day: "numeric",
+    month: "short",
+  };
+  formEl.textContent = currentDate.toLocaleDateString("en-US", options);
+}
+
+updateDateTitle();
+
+formUpdateDateTitle();
